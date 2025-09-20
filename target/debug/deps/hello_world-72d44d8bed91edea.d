7ee16e60860d1a5e43642b7606737f08f77270ba
mssngtxtrs <missingtextures970@gmail.com>
2025-09-20 18:24:42 +0300

    Removing trash after my stupidity

--- target/debug/deps/hello_world-72d44d8bed91edea.d
+++
@@ -1,5 +0,0 @@
-/home/mssngtxtrs/_projects/rust/hello-world/target/debug/deps/hello_world-72d44d8bed91edea.d: src/main.rs
-
-/home/mssngtxtrs/_projects/rust/hello-world/target/debug/deps/libhello_world-72d44d8bed91edea.rmeta: src/main.rs
-
-src/main.rs:
