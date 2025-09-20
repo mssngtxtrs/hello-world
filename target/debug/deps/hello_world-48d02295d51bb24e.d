9627b9c3f0e1de3a83db49e286b1c80870c4fa37
mssngtxtrs <missingtextures970@gmail.com>
2025-09-20 18:08:50 +0300

    Found more suitable solution for overflow problem

---
+++ target/debug/deps/hello_world-48d02295d51bb24e.d
@@ -0,0 +1,5 @@
+/home/mssngtxtrs/_projects/rust/hello-world/target/debug/deps/hello_world-48d02295d51bb24e.d: src/main.rs
+
+/home/mssngtxtrs/_projects/rust/hello-world/target/debug/deps/libhello_world-48d02295d51bb24e.rmeta: src/main.rs
+
+src/main.rs:
