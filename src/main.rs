dec428dfde63a9821d31952d2e5b20a271ec27c7
mssngtxtrs <missingtextures970@gmail.com>
2025-09-20 22:08:50 +0300

    Begin to write code for delivery system

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,44 @@
 
 
 
+//##################
+//#   STRUCTURES   #
+//##################
+
+
+
+#[derive(Debug)]
+enum OrderStatus {
+    Paid { paid_date: String },
+    Sent { paid_date: String, sent_date: String },
+    Delivered { paid_date: String, sent_date: String, deliver_date: String },
+    Disputed { paid_date: String, sent_date: String, deliver_date: String, reason: String }
+} impl OrderStatus {
+    fn info(&self) {
+        match self {
+            Self::Paid { paid_date } => { 
+                println!("{}", paid_date) 
+            },
+            Self::Sent { paid_date, sent_date } => { 
+                println!("{}\n{}", paid_date, sent_date) 
+            },
+            Self::Delivered { paid_date, sent_date, deliver_date } => { 
+                println!("{}\n{}\n{}", paid_date, sent_date, deliver_date) 
+            },
+            Self::Disputed { paid_date, sent_date, deliver_date, reason } => { 
+                println!("{}\n{}\n{}\n{}", paid_date, sent_date, deliver_date, reason) 
+            }
+        }
+    }
+}
+
+struct Order {
+    customer: String,
+    status: OrderStatus
+}
+
+
+
 //############
 //#   MAIN   #
 //############
@@ -9,8 +47,23 @@
 
 
 fn main() {
-    
+    let mut order = Order {
+        customer: "David".to_string(),
+        status: OrderStatus::Paid{ 
+            paid_date: "12.12.12".to_string()
+        }
+    };
 }
+
+
+
+//#################
+//#   FUNCTIONS   #
+//#################
+
+
+
+
 
 
 
