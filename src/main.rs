c84ef81028d39926ce56a0c0bdfe05322c023343
mssngtxtrs <missingtextures970@gmail.com>
2025-10-14 10:59:39 +0300

    Cleaning project, beginning of new snippet

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-use std::env::current_dir;
-use std::fs::create_dir_all;
-use std::path::PathBuf;
-
-fn main() {
-    let mut current_path = get_current_dir().expect("Cannot get current path!");
-
-    current_path.push("resources/");
-
-    match create_dir_in(&current_path) {
-        Ok(p) => println!("Path \"{p}\" created successfully!"),
-        Err(e) => panic!("Error: {e:?}"),
-    }
-}
-
-fn get_current_dir() -> Result<PathBuf, std::io::Error> {
-    let path = current_dir()?;
-    Ok(path)
-}
-
-fn create_dir_in(target: &PathBuf) -> Result<std::string::String, std::io::Error> {
-    match create_dir_all(target) {
-        Ok(()) => Ok(format!("{}", target.to_string_lossy())),
-        Err(e) => Err(e),
-    }
-}
+fn main() {}
