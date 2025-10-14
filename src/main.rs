e9bfc56e53624503f012d59a4083ce78d10c569c
mssngtxtrs <missingtextures970@gmail.com>
2025-10-14 10:57:24 +0300

    Moved error handling in separate functions

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,27 @@
 
 use std::env::current_dir;
 use std::fs::create_dir_all;
-use std::io::ErrorKind;
+use std::path::PathBuf;
 
 fn main() {
-    let mut current_path = match current_dir() {
-        Ok(p) => p,
-        Err(e) => panic!("Error: {e:?}"),
-    };
+    let mut current_path = get_current_dir().expect("Cannot get current path!");
 
     current_path.push("resources/");
 
-    create_dir_all(&current_path).unwrap_or_else(|e| panic!("Error: {e:?}"));
+    match create_dir_in(&current_path) {
+        Ok(p) => println!("Path \"{p}\" created successfully!"),
+        Err(e) => panic!("Error: {e:?}"),
+    }
+}
+
+fn get_current_dir() -> Result<PathBuf, std::io::Error> {
+    let path = current_dir()?;
+    Ok(path)
+}
+
+fn create_dir_in(target: &PathBuf) -> Result<std::string::String, std::io::Error> {
+    match create_dir_all(target) {
+        Ok(()) => Ok(format!("{}", target.to_string_lossy())),
+        Err(e) => Err(e),
+    }
 }
