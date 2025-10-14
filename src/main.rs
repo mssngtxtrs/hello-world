18c30155f2adcfb94740d77710f9facca506f217
mssngtxtrs <missingtextures970@gmail.com>
2025-10-14 10:02:14 +0300

    Cleaning project, begin new snippet

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-mod pseudo_generator;
-
-use pseudo_generator::random;
-
-fn main() {
-    let random = random().value;
-    println!("Hello, World!\n{random}");
-}
-
-pub fn shared() {
-    println!("shared!");
-}
+fn main() {}
--- src/pseudo_generator/random_number.rs
+++
@@ -1,8 +0,0 @@
-pub struct Rand {
-    pub value: u8,
-}
-impl Rand {
-    pub fn new(value: u8) -> Self {
-        Self { value }
-    }
-}
