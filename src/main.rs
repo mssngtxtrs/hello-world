1d8d649cd64b1ebbb5783f830370651a941db766
mssngtxtrs <missingtextures970@gmail.com>
2025-10-14 10:27:33 +0300

    Basic error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-fn main() {}
+use std::env::current_dir;
+use std::fs::create_dir_all;
+use std::io::ErrorKind;
+
+fn main() {
+    let mut current_path = match current_dir() {
+        Ok(p) => p,
+        Err(e) => panic!("Error: {e:?}"),
+    };
+
+    current_path.push("resources/");
+
+    match create_dir_all(&current_path) {
+        Ok(()) => println!("Path {} created successfully!", current_path.display()),
+        Err(e) => match e.kind() {
+            ErrorKind::InvalidFilename => panic!("Invalid filename!"),
+            _ => panic!("unknown!"),
+        },
+    }
+}
