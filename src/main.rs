bf6347eb802cf84cfb17dc66211f646574c467fa
mssngtxtrs <missingtextures970@gmail.com>
2025-10-16 09:21:15 +0300

    Types in structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-fn main() {}
+use std::ops::Add;
+
+//fn sum<T>(numbers: &[T]) -> T {
+//    numbers.iter().copied().reduce(|acc, n| acc + n).unwrap()
+//}
+
+#[derive(Debug)]
+struct Employee<T, K> {
+    age: T,
+    salary: K,
+    tax: K,
+}
+
+fn main() {
+    //let int_1: Vec<i32> = vec![1, 2, 3, 4];
+    //let int_2: Vec<i8> = vec![63, 127, 0];
+    //
+    //let result_1 = sum(&int_1);
+    //let result_2 = sum(&int_2);
+
+    let employee = Employee {
+        age: 30,
+        salary: 30000.,
+        tax: 5000.,
+    };
+
+    println!("{employee:?}");
+}
